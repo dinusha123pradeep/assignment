6ba02ac512c680353faeecd661429248ce9fca5b
Dinusha Marathunga <dinusha123pradeep@gmail.com>
2023-01-10 22:26:59 +0800

    Improvements

--- main.java
+++ main.java
@@ -1,36 +1,62 @@
+import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
 class HelloWorld {
     public static void main(String[] args) {
-        spicyChicken(1, 100);
+        try {
+            spicyChicken(1, 100);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
         calculateTaxAmount(180001);
+        calculateTaxAmount(37009);
+        calculateTaxAmount(37001);
+        calculateTaxAmount(1);
     } 
 
-    private static void spicyChicken(int min, int max) {
+    /**
+     * Generate spicyChicken output using min and max integer  
+     * @param min
+     * @param max
+     * @throws Exception
+     */
+    private static void spicyChicken(int min, int max) throws Exception {
 
-        String spicy = "Spicy";
-        String chichekn = "Chicken";
+        // Define constant values
+        final String SPICY = "Spicy";
+        final String CHICHKEN = "Chicken";
 
+        // Validate input parameters
+        if (min > max) {
+            throw new Exception("Min value cannot greater than Max value. Min : " + min + " Max : " + max);
+        }
+
+        // Loop through the given range
         for(int i = min; i <= max; i++) {
             String resultString = "";
 
             if (i % 3 == 0) {
-                resultString += spicy;
+                resultString += SPICY;
             }
 
             if (i % 5 == 0) {
-                resultString += chichekn;
+                resultString += CHICHKEN;
             }
 
             System.out.println(i + " " + resultString);
         }
-        
     }
 
+    /**
+     * calculate Tax amount for given income
+     * @param income
+     */
     private static void calculateTaxAmount(double income) {
         double band;
         double totalTax = 0;
 
+        // Calculate total payable tax for each bands
         if (income > 180000) {
             band = 180000;
             totalTax += (income - band) * 0.45;
@@ -54,9 +80,10 @@ class HelloWorld {
             totalTax += (income - band) * 0.19;
         }
 
-        DecimalFormat df = new DecimalFormat("#.##");
-        totalTax = Double.valueOf(df.format(totalTax));
+        // Round DOWN total tax in to two decimal points
+        DecimalFormat df = new DecimalFormat("0.00");
+        df.setRoundingMode(RoundingMode.DOWN);
 
-        System.out.println(totalTax);
+        System.out.println(df.format(totalTax));
     }
 }
